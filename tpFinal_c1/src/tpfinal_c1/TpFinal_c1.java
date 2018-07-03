9561274acfb48fa4b1b97939e3cb9489be8c2961
pingino <pingino@mininterior.gob.ar>
2018-07-03 12:44:12 -0300

    se termino el ejercicio C1

--- tpFinal_c1/src/tpfinal_c1/TpFinal_c1.java
+++ tpFinal_c1/src/tpfinal_c1/TpFinal_c1.java
@@ -18,17 +18,28 @@ public class TpFinal_c1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        //SE DECLARAN LOS SCANNER PARA VALIDAR LA SALIDA Y PARA LA CAPTURA DEL CARACTER
+        Scanner salida = new Scanner(System.in);
         Scanner car = new Scanner(System.in);
+        String exit;
         char var;
         System.out.println("Este programa lee un caracter por teclado y evalua si es una letra mayuscula");
+        //INICIO DEL CICLO QUE VALIDA SI EL USUARIO QUIERE SALIR DEL PROGRAMA
+        do {
         System.out.print("Ingrese el caracter a evaluar: ");
         var = car.next().charAt(0);
+        //VALIDACION SI ES LATRA Y MAYUSCULA CON METODO isUpperCase
         if(Character.isUpperCase(var)){
+            //SI ENTRA ACA INFORMA QUE ES UNA LETRA MAYUSCULA
             System.out.println("El caracter " + var + " es una letra mayuscula");
         }else{
+            //INFORMA QUE NO ES LETRA MAYUSCULA
             System.out.println("El caracter "+ var +" no es una letra mayuscula");
         }
-        
+        //PREGUNTA AL USUARIO SI QUIERE SALIR DEL PROGRAMA
+        System.out.println("Desea salir de la aplicacion? (Debe ingresar si o no)");
+        exit = salida.nextLine();
+        } while(!"si".equalsIgnoreCase(exit));
     }
     
 }
