aa3add155271646b250efaff1ab17033e71598cb
Pablo Ingino <pablo.ingino@gmail.com>
2018-07-04 19:21:31 -0300

    se termino el d7

--- tpFinal_d5/src/tpfinal_d5/TpFinal_d5.java
+++ tpFinal_d5/src/tpfinal_d5/TpFinal_d5.java
@@ -20,16 +20,28 @@ public class TpFinal_d5 {
         //TODO code application logic here
         //SE DECLARAN LOS SCANNER PARA VALIDAR LA SALIDA Y PARA LA CAPTURA DEL CARACTER
         Scanner salida = new Scanner(System.in);
-        Scanner num = new Scanner(System.in);
+        Scanner nom = new Scanner(System.in);
+        Scanner sue = new Scanner(System.in);
         String exit;
-        int[] arreglo = new int[10];
+        String[] nombre = new String[20];
+        float[] sueldo = new float[20];
         int x=0;
         System.out.println("Este programa guarda los 10 primeros numeros pares en un array de 10 elementos");
         
         //INICIO DEL CICLO QUE VALIDA SI EL USUARIO QUIERE SALIR DEL PROGRAMA
         do {
+            for(int i = 0; i < 20; i++){
+                System.out.print("Ingrese el nombre del empleado: ");
+                nombre[i]=nom.nextLine();
+                System.out.print("Ingrese el sueldo del empleado: ");
+                sueldo[i]=sue.nextFloat();
+                System.out.println("-----------------------------------------");
+            }
             
-                  
+            for(int i = 0; i < 20; i++){
+                System.out.println("Nombre: "+nombre[i]+ "         | " + " Sueldo: " +sueldo[i] +" | ");
+                System.out.println("-----------------------------------------");
+            }      
            
         //PREGUNTA AL USUARIO SI QUIERE SALIR DEL PROGRAMA
         System.out.println("Desea salir de la aplicacion? (Debe ingresar si o no)");
